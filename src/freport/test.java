ff65917f32e508c7450967204df027675ff0c192
javays <zhujun.it@qq.com>
2016-01-25 16:57:47 +0800

    first commit

---
+++ src/freport/test.java
@@ -0,0 +1,30 @@
+/* 
+ * Copyright (c) 2016, S.F. Express Inc. All rights reserved.
+ */
+
+package freport;
+
+/**
+ * 描述：
+ * 
+ * <pre>HISTORY
+ * ****************************************************************************
+ *  ID   DATE           PERSON          REASON
+ *  1    2016年1月25日      Steven.Zhu         Create
+ * ****************************************************************************
+ * </pre>
+ * @author Steven.Zhu
+ * @since 
+ */
+
+public class test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
